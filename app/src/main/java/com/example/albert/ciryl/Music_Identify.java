85de41f42a57f5088f4a0cb3959e49cd9faebd06
yiliang6 <yiliang6@illinois.edu>
2018-12-01 17:26:10 -0600

    Finished basic User Interface

---
+++ app/src/main/java/com/example/albert/ciryl/Music_Identify.java
@@ -0,0 +1,14 @@
+package com.example.albert.ciryl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Music_Identify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music__identify);
+        System.out.println("hello");
+    }
+}
