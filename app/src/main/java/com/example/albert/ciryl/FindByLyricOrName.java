a9e68966eb0267f9876599849b61c0aa4b785a8d
yiliang6 <yiliang6@illinois.edu>
2018-12-06 00:57:52 -0600

    Deleted unnecessary function

--- app/src/main/java/com/example/albert/ciryl/FindByLyricOrName.java
+++ app/src/main/java/com/example/albert/ciryl/FindByLyricOrName.java
@@ -17,8 +17,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+//import com.google.gson.Gson;
+//import com.google.gson.JsonObject;
 
 import org.json.JSONObject;
 
