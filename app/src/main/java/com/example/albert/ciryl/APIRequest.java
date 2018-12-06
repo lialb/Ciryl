f0f547234f9d8c14d78ff1a57b727ffaf7a8a300
yiliang6 <yiliang6@illinois.edu>
2018-12-05 23:45:26 -0600

    Got API to work

--- app/src/main/java/com/example/albert/ciryl/APIRequest.java
+++ app/src/main/java/com/example/albert/ciryl/APIRequest.java
@@ -1,42 +1,27 @@
 package com.example.albert.ciryl;
 
-import android.app.Activity;
-import android.app.VoiceInteractor;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import android.support.v7.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.Volley;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.net.ssl.HttpsURLConnection;
-
-public class FindByLyricOrName extends Activity {
-    TextView textView;
-    public static String track_id = "71676331";
-    public static String apikey = "55209121330a9e50d5bc48132c055b8d";
+/**
+ * Main screen for our API testing app.
+ */
+public final class APIRequest extends AppCompatActivity {
+    /** Default logging tag for messages from the main activity. */
     private static final String TAG = "Lab12:Main";
 
     /** Request queue for our network requests. */
@@ -55,7 +40,7 @@ public class FindByLyricOrName extends Activity {
         requestQueue = Volley.newRequestQueue(this);
 
         // Load the main layout for our activity
-        setContentView(R.layout.activity_find_by_lyric_or_name);
+        setContentView(R.layout.activity_main);
 
         // Attach the handler to our UI button
         final Button startAPICall = findViewById(R.id.SearchButton);
@@ -66,8 +51,6 @@ public class FindByLyricOrName extends Activity {
                 startAPICall();
             }
         });
-
-
     }
 
     /**
@@ -77,8 +60,7 @@ public class FindByLyricOrName extends Activity {
         try {
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                     Request.Method.GET,
-                    "https://api.musixmatch.com/ws/1.1/track.lyrics.get?fo" +
-                            "rmat=json&callback=callback&track_id=" + track_id + "&apikey=" + apikey,
+                    "https://api.musixmatch.com/ws/1.1/",
                     null,
                     new Response.Listener<JSONObject>() {
                         @Override
@@ -90,22 +72,12 @@ public class FindByLyricOrName extends Activity {
                 public void onErrorResponse(final VolleyError error) {
                     Log.w(TAG, error.toString());
                 }
-            }) {
-                @Override
-                public Map<String, String> getHeaders() {
-                    Map<String, String>  params = new HashMap<String, String>();
-                    params.put("apikey", "55209121330a9e50d5bc48132c055b8d");
-                    Log.d(TAG, params.toString());
-                    return params;
-                }
-            };
+            });
             requestQueue.add(jsonObjectRequest);
         } catch (Exception e) {
             e.printStackTrace();
-        }/* finally {
-            textView.setText("smt");
-        }*/
+        }
 
     }
-
 }
+
