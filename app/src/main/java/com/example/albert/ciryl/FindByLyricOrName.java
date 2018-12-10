49f6c01e11f473361982e9d95bff931cfebf0cf4
yiliang6 <yiliang6@illinois.edu>
2018-12-10 16:29:17 -0600

    Added User Input Function On searching pages

--- app/src/main/java/com/example/albert/ciryl/FindByLyricOrName.java
+++ app/src/main/java/com/example/albert/ciryl/FindByLyricOrName.java
@@ -1,11 +1,15 @@
 package com.example.albert.ciryl;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.VoiceInteractor;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EdgeEffect;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -20,6 +24,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 //import com.google.gson.Gson;
 //import com.google.gson.JsonObject;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -28,6 +33,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,13 +42,15 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class FindByLyricOrName extends Activity {
     TextView textView;
+    //Button button;
+    private TextInputLayout userInput;
+
     public static String track_id = "71676331";
-    public static String apikey = "55209121330a9e50d5bc48132c055b8d";
-    private static final String TAG = "Lab12:Main";
+    public static String apiKey = "55209121330a9e50d5bc48132c055b8d";
+    private static final String TAG = "Ciryl";
 
     /** Request queue for our network requests. */
     private static RequestQueue requestQueue;
-
     /**
      * Run when our activity comes into view.
      *
@@ -57,19 +66,36 @@ public class FindByLyricOrName extends Activity {
         // Load the main layout for our activity
         setContentView(R.layout.activity_find_by_lyric_or_name);
 
+        textView = (TextView) findViewById(R.id.textView);
+
+        userInput = findViewById(R.id.userSearchInput);
+
         // Attach the handler to our UI button
         final Button startAPICall = findViewById(R.id.SearchButton);
         startAPICall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
+
+
                 Log.d(TAG, "Start API button clicked");
                 startAPICall();
             }
         });
-
-
     }
-
+    private String trimSearch(String input) {
+        String url = "";
+        String[] arr = input.split(" ");
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(arr));
+        for (int i  = 0; i < list.size(); i++) {
+            if (list.get(i).length() > 0) {
+                url += list.get(i);
+                if (i < list.size() - 1) {
+                    url += "%20";
+                }
+            }
+        }
+        return url;
+    }
     /**
      * Make an API call.
      */
@@ -78,12 +104,23 @@ public class FindByLyricOrName extends Activity {
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                     Request.Method.GET,
                     "https://api.musixmatch.com/ws/1.1/track.lyrics.get?fo" +
-                            "rmat=json&callback=callback&track_id=" + track_id + "&apikey=" + apikey,
+                            "rmat=json&callback=callback&track_id=" + track_id + "&apikey=" + apiKey,
                     null,
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(final JSONObject response) {
                             Log.d(TAG, response.toString());
+                            String searchParameters = userInput.getEditText().getText().toString().trim();
+                            searchParameters = "smt" + trimSearch(searchParameters);
+                            textView.setText(searchParameters);
+                           /* try {
+                                JSONObject lyric = response.getJSONObject("lyrics_body");
+                               // String searchParameters = userInput.getEditText().getText().toString().trim();
+                                searchParameters = "smt" + trimSearch(searchParameters);
+                                textView.setText(searchParameters);//getString("lyrics_body"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }*/
                         }
                     }, new Response.ErrorListener() {
                 @Override
@@ -94,7 +131,7 @@ public class FindByLyricOrName extends Activity {
                 @Override
                 public Map<String, String> getHeaders() {
                     Map<String, String>  params = new HashMap<String, String>();
-                    params.put("apikey", "55209121330a9e50d5bc48132c055b8d");
+                    params.put("apiKey", "55209121330a9e50d5bc48132c055b8d");
                     Log.d(TAG, params.toString());
                     return params;
                 }
